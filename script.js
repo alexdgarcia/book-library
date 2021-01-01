86b79e5bac300bae3f52c61a21a73d923ab6d486
Alex Dominguez Garcia <alexand.dominguez@gmail.com>
2021-01-01 15:27:48 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,20 @@
+let myLibrary = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    pages: 300,
+    isRead: false
+  },
+];
+
+function Book(title, author, pages, isRead) {
+  this.title  = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+
+
+}
